c78bdea4213d1cf6d2f71ab97dccedd85056bb5f
Lillian Hwang-Geddes <lillian.hwang-geddes@viam.com>
2026-02-26 13:32:05 -0500

    remove incorrect import

--- src/main.py
+++ src/main.py
@@ -2,8 +2,6 @@ import asyncio
 from viam.module.module import Module
 from models.hackathon_greenhouse_demo import HackathonGreenhouseDemo
 
-# TODO fix this import
-from models.stevebriskin:scd4x:scd4x import Scd4x
 
 if __name__ == '__main__':
     asyncio.run(Module.run_from_registry())
