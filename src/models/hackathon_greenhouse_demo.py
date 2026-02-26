ef7cd71183530ed28a7934f094eb94b150312068
Lillian Hwang-Geddes <lillian.hwang-geddes@viam.com>
2026-02-26 13:31:39 -0500

    validate config

--- src/models/hackathon_greenhouse_demo.py
+++ src/models/hackathon_greenhouse_demo.py
@@ -50,7 +50,22 @@ class HackathonGreenhouseDemo(Generic, EasyResource):
                 first element is a list of required dependencies and the
                 second element is a list of optional dependencies
         """
-        return [], []
+        req_deps = []
+        fields = config.attributes.fields
+
+        if "gas_sensor_name" not in fields:
+            raise Exception("missing required gas_sensor_name attribute")
+        elif not fields["gas_sensor_name"].HasField("string_value"):
+            raise Exception("gas_sensor_name must be a string")
+        gas_sensor_name = fields["gas_sensor_name"].string_value
+        if not gas_sensor_name:
+            raise ValueError("gas_sensor_name cannot be empty")
+        req_deps.append(gas_sensor_name)
+
+        return req_deps, []
+    
+    def reconfigure(self, config: ComponentConfig, dependencies: Mapping[ResourceName, ResourceBase]):
+        pass
 
     async def do_command(
         self,
